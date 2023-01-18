f9a7ee9dff3927a51817ab4b2a5b0b7f01901f34
hursung1 <hwan4216@gmail.com>
2023-01-19 01:18:16 +0900

    20230119:0118

--- not_solved/9663.py
+++ not_solved/9663.py
@@ -1,4 +1,7 @@
 from copy import deepcopy
+
+done_position = []
+
 def set_false(chess_map, i, j):
     _map = deepcopy(chess_map)
     for idx in range(len(chess_map)):
@@ -19,21 +22,30 @@ def set_false(chess_map, i, j):
     return _map
 
     
-def n_queen(N, n, chess_map, cnt):
+def n_queen(N, n, chess_map, cnt, position):
     for i in range(N):
         for j in range(N):
             if chess_map[i][j]:
+                position.append((i, j))
                 if n == 1:
                     cnt += 1
-                    print("!!")
+                    print(done_position)
+                    print(position)
+                    if not position in done_position:
+                        done_position.append(position)
+                    # print("!!")
                 else:
                     # set false for impossible region
                     new_map = set_false(chess_map, i, j)
-                    n_queen(N, n-1, new_map, cnt)
+                    # print(new_map)
+                    cnt = n_queen(N, n-1, new_map, cnt, position)
+                
+                del position[-1]
+
 
     return cnt
 
 N = int(input())
 chess_map = [[True for __ in range(N)] for _ in range(N)]
 possible_cnt = 0
-print(n_queen(N, N, chess_map, possible_cnt))
+print(n_queen(N, N, chess_map, possible_cnt, []))
